a80590f4fd0ba357d9f8a415a67c57be04f25fd0
NoobyJuli <62073602+NoobyJuli@users.noreply.github.com>
2023-12-19 20:15:48 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,6 +32,14 @@ export default function Index() {
 Vote for GlobalShare</a>
   </Link>
         </div>
+	      
+        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
+        <Image width="600" height="600" src="/img/newlogo.png"  />
+        </div>
+        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag opacity-10 pointer-events-none -right-[15vw] absolute top-[13vw] h-[90vw] grayscale -rotate-9">
+        <Image  width="600" height="600" src="/img/newlogo.png"  />
+        </div>
+      </div>
 
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
