4b53ca99c5caef8a71d2fadfa1bc3e5980cd5dfe
NoobyJuli <62073602+NoobyJuli@users.noreply.github.com>
2023-12-29 00:10:20 +0100

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,21 +1,5 @@
 export default function Tos() {
   return (
-    <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Service</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 19/12/2023</p>
-  <div className="animateHeader text-white mt-5">
-<p>⊳ Every discord server can use our advertising service for free.</p>
-<div className="py-3"></div>
-<p>⊳ It's strictly forbidden using GlobalShare or any other of our bots for producing spam or other dumb shit.</p>
-<div className="py-3"></div>
-<p>⊳ Setting up 18+ servers on GlobalShare is also strictly against our tos and will be punished with a permanent ban.</p>
-<div className="py-3"></div>
-<p>⊳ Using phishing links or other redirecting methods to bad webpages is also forbidden.</p>
-<div className="py-3"></div>
-<p>⊳ Spamming commands (such as the report tool) will automatically cause a permanent ban from our services.</p>
-<div className="py-3"></div>
-<p>⊳ In order to execute some commands, you may have to vote for <a className="text-white" href="https://top.gg/bot/1100500027840929963/vote">GlobalShare</a> sometimes.</p>
-</div>
       
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
