c34dcb8e37d191ca81a961e7c74ef8677aad9ae2
NoobyJuli <62073602+NoobyJuli@users.noreply.github.com>
2023-12-28 23:30:50 +0100

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -19,7 +19,7 @@ export default function Tos() {
       
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
-                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
+                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
 
                   <p className="text-3xl text-white font-semibold">Acceptance of Terms</p>
                   <br/>
