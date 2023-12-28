4208af66f0af23ccc3034d769f556cba8e509494
NoobyJuli <62073602+NoobyJuli@users.noreply.github.com>
2023-12-28 23:06:34 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -82,16 +82,7 @@ Vote for GlobalShare</a>
 
 
 
-      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
-              <>
-                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                 
-                  <p className="text-3xl text-white font-semibold">🏆 Best Advertising Bot in 2023</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">The wide range of set-up options and simple design of our bot make GlobalShare the perfect advertising bot for your Discord server!</p>
-                </div>
 
-              </>
-        </div>
 
       </>
   )
