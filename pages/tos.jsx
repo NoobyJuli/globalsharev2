30daf64af3b3311457ddfe842a0ec811845e7be9
NoobyJuli <62073602+NoobyJuli@users.noreply.github.com>
2023-12-29 01:12:02 +0100

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -53,6 +53,19 @@ export default function Tos() {
                 </div>
               </>
         </div>
+
+            <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
+              <>
+                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
+
+                  <p className="text-3xl text-white font-semibold">Contact</p>
+                  <br/>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Please do not hesitate to contact us if you have any questions regarding the privacy policy or the terms of use of our services. <br/>
+- E-mail: globalshareservices@gmail.com <br/>
+- Discord: discord.gg/8qCVhssaXY</p>
+                </div>
+              </>
+        </div>
       
     </div>
   );
